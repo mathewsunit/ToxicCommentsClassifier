4c569edd85da186fe65a6f23d9a4462611aa49d0
Sunit Mathew <mathewsunit@gmail.com>
2018-04-24 11:50:15 -0500

    Bug Fixes for MLP

--- src/main/scala/toxic/MainObj.scala
+++ src/main/scala/toxic/MainObj.scala
@@ -26,9 +26,9 @@ object MainObj {
     println("Number of rows in training data: " + trainSet.count())
     println("Number of rows in test data: " + testSet.count())
     println("Splitting training data to 75% training and 25% testing data")
-    val Array(trainData, testData) = trainSet.randomSplit(Array(0.9, 0.1), seed = 12345)
-    buildPredictUsingNB(sc, trainData, testData, testSet, args(2).toString)
-    //buildPredictUsingMLP(trainData, testData, testSet)
+    val Array(trainData, testData) = trainSet.randomSplit(Array(0.75, 0.25), seed = 12345)
+//    buildPredictUsingNB(sc, trainData, testData, testSet, args(2).toString)
+    buildPredictUsingMLP(sc, trainData, testData, testSet,  args(2).toString)
   }
 
   def trueValue = true
@@ -61,8 +61,8 @@ object MainObj {
   def getMLPModel(dataFrame: DataFrame): PipelineModel = {
     val tokenizer = new Tokenizer().setInputCol("comment_text").setOutputCol("words")
     val stopWordsRemover = new StopWordsRemover().setInputCol("words").setOutputCol("filtered")
-    val hashingTF = new HashingTF().setInputCol(tokenizer.getOutputCol).setOutputCol("features")
-    val layers = Array[Int](10, 5, 5, 2)
+    val hashingTF = new HashingTF().setInputCol(stopWordsRemover.getOutputCol).setOutputCol("features").setNumFeatures(20)
+    val layers = Array[Int](hashingTF.getNumFeatures, 10, 5, 2)
     val mlp = new MultilayerPerceptronClassifier().setLayers(layers).setMaxIter(100)
     val pipeline = new Pipeline().setStages(Array(tokenizer, stopWordsRemover, hashingTF, mlp))
     pipeline.fit(dataFrame)
@@ -126,14 +126,67 @@ object MainObj {
     bw.write(SparkUtils.evaluateModel(nbPredicted.select("insult", "insult_predicted").rdd.map{ case Row(prediction: Double, label: Double) => (prediction, label) },"Insult"))
     bw.write(SparkUtils.evaluateModel(nbPredicted.select("identity_hate", "identity_hate_predicted").rdd.map{ case Row(prediction: Double, label: Double) => (prediction, label) },"Identity Hate"))
     bw.close()
-    /*val metrics = new MulticlassMetrics(nbPredicted.select("toxic", "toxic_predicted").rdd
-    .flatMap(row => row.getDouble(0))
-    println(metrics.accuracy)
-    println(metrics.confusionMatrix)*/
-    nbPredicted.show()
+//    val metrics = new MulticlassMetrics(nbPredicted.select("toxic", "toxic_predicted").rdd
+//    .flatMap(row => row.getDouble(0))
+//    println(metrics.accuracy)
+//    println(metrics.confusionMatrix)
+//    nbPredicted.show()
   }
 
-  def buildPredictUsingMLP(trainData: DataFrame, testData: DataFrame, testSet: DataFrame): Unit = {
+  def writeToOutPut(sc: SparkSession, trainData: DataFrame, testData: DataFrame, testSet: DataFrame,
+                          destPath: String): Unit = {
+    println("Building Naive Bayes model")
+    val toxicNBModel = getNBModel(trainData.withColumnRenamed("toxic", "label"))
+    val toxicPredictions = getPredictionForModel(toxicNBModel, testData, "toxic_predicted")
+    val severeToxicNBModel = getNBModel(trainData.withColumnRenamed("severe_toxic",
+      "label"))
+    val severeToxicPredictions = getPredictionForModel(severeToxicNBModel, testData,
+      "severe_toxic_predicted")
+    val obsceneNBModel = getNBModel(trainData.withColumnRenamed("obscene", "label"))
+    val obscenePredictions = getPredictionForModel(obsceneNBModel, testData, "obscene_predicted")
+    val threatNBModel = getNBModel(trainData.withColumnRenamed("threat", "label"))
+    val threatPredictions = getPredictionForModel(threatNBModel, testData, "threat_predicted")
+    val insultNBModel = getNBModel(trainData.withColumnRenamed("insult", "label"))
+    val insultPredictions = getPredictionForModel(insultNBModel, testData, "insult_predicted")
+    val identityHateNBModel = getNBModel(trainData.withColumnRenamed("identity_hate",
+      "label"))
+    val identityHatePredictions = getPredictionForModel(identityHateNBModel, testData,
+      "identity_hate_predicted")
+    println("Predicting using Naive Bayes model")
+    val nbPredicted = joinPredictions(testData, toxicPredictions, severeToxicPredictions,
+      obscenePredictions, threatPredictions, insultPredictions, identityHatePredictions)
+
+    val fs = FileSystem.get(sc.sparkContext.hadoopConfiguration)
+    val path: Path = new Path(destPath+"/NBAccuracy.txt")
+    if (fs.exists(path)) {
+      fs.delete(path, true)
+    }
+    val dataOutputStream: FSDataOutputStream = fs.create(path)
+    val bw: BufferedWriter = new BufferedWriter(new OutputStreamWriter(dataOutputStream, "UTF-8"))
+
+    printAccuracy(nbPredicted, "toxic", bw)
+    printAccuracy(nbPredicted, "severe_toxic", bw)
+    printAccuracy(nbPredicted, "obscene", bw)
+    printAccuracy(nbPredicted, "threat", bw)
+    printAccuracy(nbPredicted, "insult", bw)
+    printAccuracy(nbPredicted, "identity_hate", bw)
+    //please try the below code. We need to print the confusion matrix for this using MulticlassMetrics
+    bw.write(SparkUtils.evaluateModel(nbPredicted.select("toxic", "toxic_predicted").rdd.map{ case Row(prediction: Double, label: Double) => (prediction, label) },"Toxic"))
+    bw.write(SparkUtils.evaluateModel(nbPredicted.select("severe_toxic", "severe_toxic_predicted").rdd.map{ case Row(prediction: Double, label: Double) => (prediction, label) },"Severe Toxic"))
+    bw.write(SparkUtils.evaluateModel(nbPredicted.select("obscene", "obscene_predicted").rdd.map{ case Row(prediction: Double, label: Double) => (prediction, label) },"Obscene"))
+    bw.write(SparkUtils.evaluateModel(nbPredicted.select("threat", "threat_predicted").rdd.map{ case Row(prediction: Double, label: Double) => (prediction, label) },"Threat"))
+    bw.write(SparkUtils.evaluateModel(nbPredicted.select("insult", "insult_predicted").rdd.map{ case Row(prediction: Double, label: Double) => (prediction, label) },"Insult"))
+    bw.write(SparkUtils.evaluateModel(nbPredicted.select("identity_hate", "identity_hate_predicted").rdd.map{ case Row(prediction: Double, label: Double) => (prediction, label) },"Identity Hate"))
+    bw.close()
+    //    val metrics = new MulticlassMetrics(nbPredicted.select("toxic", "toxic_predicted").rdd
+    //    .flatMap(row => row.getDouble(0))
+    //    println(metrics.accuracy)
+    //    println(metrics.confusionMatrix)
+    //    nbPredicted.show()
+  }
+
+  def buildPredictUsingMLP(sc: SparkSession, trainData: DataFrame, testData: DataFrame, testSet: DataFrame,
+                           destPath: String): Unit = {
     println("Building MLP model")
     val toxicMLPModel = getMLPModel(trainData.withColumnRenamed("toxic", "label"))
     val toxicPredictions = getPredictionForModel(toxicMLPModel, testData, "toxic_predicted")
@@ -155,7 +208,30 @@ object MainObj {
     println("Predicting using MLP model")
     val mlpPredicted = joinPredictions(testData, toxicPredictions, severeToxicPredictions,
       obscenePredictions, threatPredictions, insultPredictions, identityHatePredictions)
-    mlpPredicted.show()
+//    mlpPredicted.show()
+
+    val fs = FileSystem.get(sc.sparkContext.hadoopConfiguration)
+    val path: Path = new Path(destPath+"/MLBAccuracy.txt")
+    if (fs.exists(path)) {
+      fs.delete(path, true)
+    }
+    val dataOutputStream: FSDataOutputStream = fs.create(path)
+    val bw: BufferedWriter = new BufferedWriter(new OutputStreamWriter(dataOutputStream, "UTF-8"))
+
+    printAccuracy(mlpPredicted, "toxic", bw)
+    printAccuracy(mlpPredicted, "severe_toxic", bw)
+    printAccuracy(mlpPredicted, "obscene", bw)
+    printAccuracy(mlpPredicted, "threat", bw)
+    printAccuracy(mlpPredicted, "insult", bw)
+    printAccuracy(mlpPredicted, "identity_hate", bw)
+    //please try the below code. We need to print the confusion matrix for this using MulticlassMetrics
+    bw.write(SparkUtils.evaluateModel(mlpPredicted.select("toxic", "toxic_predicted").rdd.map{ case Row(prediction: Double, label: Double) => (label, prediction) },"Toxic"))
+    bw.write(SparkUtils.evaluateModel(mlpPredicted.select("severe_toxic", "severe_toxic_predicted").rdd.map{ case Row(prediction: Double, label: Double) => (label, prediction) },"Severe Toxic"))
+    bw.write(SparkUtils.evaluateModel(mlpPredicted.select("obscene", "obscene_predicted").rdd.map{ case Row(prediction: Double, label: Double) => (label, prediction) },"Obscene"))
+    bw.write(SparkUtils.evaluateModel(mlpPredicted.select("threat", "threat_predicted").rdd.map{ case Row(prediction: Double, label: Double) => (label, prediction) },"Threat"))
+    bw.write(SparkUtils.evaluateModel(mlpPredicted.select("insult", "insult_predicted").rdd.map{ case Row(prediction: Double, label: Double) => (label, prediction) },"Insult"))
+    bw.write(SparkUtils.evaluateModel(mlpPredicted.select("identity_hate", "identity_hate_predicted").rdd.map{ case Row(prediction: Double, label: Double) => (label, prediction) },"Identity Hate"))
+    bw.close()
   }
 
   def printAccuracy(predicted: DataFrame, toxicType: String, bw: BufferedWriter): Unit = {
